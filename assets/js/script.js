993c4822ff0b17969e7b27cf0aed23f5e7596604
joni <codingmylife0@gmail.com>
2024-04-30 00:53:53 +0530

    upate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 
 
+function homeSectionAnimation() {
+    gsap.to(".videodiv", {
+        scrollTrigger: {
+            trigger: ".home",
+            start: "top top",
+            end: "bottom top",
+            markers: true,
+            scrub: 3,
+            pin: true
+        },
+        '--clip': "0%",
+        ease: Power2,
+        duration: 2,
+
+    })
+
+
+}
+
+homeSectionAnimation()
+
 function horizontalScrollbar() {
     let horizontalSection = document.querySelector('.model-container');
     gsap.to('.model-container', {
@@ -21,25 +42,28 @@ horizontalScrollbar()
 
 
 
-function teamsectionEffect() { }
-let teamelem = document.querySelectorAll(".team-elem");
-teamelem.forEach((val) => {
-    const childNode = val.childNodes[3];
-    console.log(childNode)
-    val.addEventListener("mouseenter", () => {
-        childNode.style.opacity = "1";
-        childNode.style.transform = "scale(1)";
-        childNode.style.top = "-9px";
-        childNode.style.left = "40%"
+function teamsectionEffect() {
+    let teamelem = document.querySelectorAll(".team-elem");
+    teamelem.forEach((val) => {
+        const childNode = val.childNodes[3];
+        console.log(childNode)
+        val.addEventListener("mouseenter", () => {
+            childNode.style.opacity = "1";
+            childNode.style.transform = "scale(1)";
+            childNode.style.top = "-9px";
+            childNode.style.left = "40%"
+        })
+
+        val.addEventListener("mouseleave", () => {
+            childNode.style.opacity = "0";
+            childNode.style.transform = "scale(0)";
+        })
+
+
+
+
     })
+}
 
-    val.addEventListener("mouseleave", () => {
-        childNode.style.opacity = "0";
-        childNode.style.transform = "scale(0)";
-    })
-
-
-
-
-})
+teamsectionEffect()
 
